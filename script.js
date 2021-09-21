e2fd2e4b0095c134e4ea0b0c5bc1ca9113f29342
Itsy0507 <itsyfelix0507@gmail.com>
2021-09-21 04:26:35 -0500

    DOM

---
+++ script.js
@@ -0,0 +1,16 @@
+var Pelements = document.getElementsByTagName ("p");
+var text1 = document.getElementById ("p1", "p2");
+text1.setAttribute("align", "left");
+
+var paragraph = document.createElement("p");
+var text2 = document.createTextNode("Wholesale and retail beans available!");
+paragraph.setAttribute ("align", "right");
+paragraph.appendChild(text2);
+document.getElementById("Text").appendChild(paragraph);
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "French Vanilla,Caramel Macchiato, Pumpkin Spice, Mocha and Hazelnut.";
+  }
+
+  photo.setAttribute ("align", "right");
+
